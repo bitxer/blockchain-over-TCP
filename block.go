a37cff6c810e37fa66d66f5f4b8c6ea981fd79f4
bitxer <24569832+bitxer@users.noreply.github.com>
2020-03-10 00:17:24 +0800

    added serialisation and deserialisation

--- block.go
+++ block.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
 	"strconv"
 )
 
@@ -13,12 +15,54 @@ type Block struct {
 	hash       []byte
 }
 
-func (b *Block) genHash() []byte {
+func (b *Block) genHash() {
 	hash := sha256.New()
 	key := strconv.Itoa(b.index)
 	key += strconv.Itoa(b.timestamp)
 	key += b.data
 	key += string(b.parentHash)
 	hash.Write([]byte(key))
-	return hash.Sum(nil)
+	b.hash = hash.Sum(nil)
+}
+
+func (b *Block) serialise() *bytes.Buffer {
+	ex := make(map[string]string)
+	ex["index"] = strconv.Itoa(b.index)
+	ex["timestamp"] = strconv.Itoa(b.timestamp)
+	ex["data"] = b.data
+	ex["parentHash"] = string(b.parentHash)
+	ex["hash"] = string(b.hash)
+	buf := new(bytes.Buffer)
+
+	en := gob.NewEncoder(buf)
+	err := en.Encode(ex)
+	if err != nil {
+		panic(err)
+	}
+	return buf
+}
+
+func deserialise(ser *bytes.Buffer) Block {
+	im := make(map[string]string)
+	de := gob.NewDecoder(ser)
+
+	err := de.Decode(&im)
+	if err != nil {
+		panic(err)
+	}
+
+	index, err := strconv.Atoi(im["index"])
+	if err != nil {
+		panic(err)
+	}
+
+	timestamp, err := strconv.Atoi(im["timestamp"])
+	if err != nil {
+		panic(err)
+	}
+
+	data := im["data"]
+	parentHash := []byte(im["parentHash"])
+	hash := []byte(im["hash"])
+	return Block{index: index, timestamp: timestamp, data: data, parentHash: parentHash, hash: hash}
 }
--- main.go
+++ main.go
@@ -5,7 +5,15 @@ import (
 )
 
 func main() {
-	block1 := Block{index: 1, timestamp: 1, data: "data"}
-	h := block1.genHash()
-	fmt.Printf("%x", h)
+	block1 := Block{index: 1, timestamp: 1, data: "data", parentHash: []byte{0}}
+	block1.genHash()
+	chain := []Block{block1}
+	chain = chain
+	be := block1.serialise()
+
+	fmt.Printf("%s\n", be)
+
+	fmt.Printf("%x\n", block1)
+	block2 := deserialise(be)
+	fmt.Printf("%x\n", block2)
 }
